5b1918331b2ea1293dc0ce993c7be5a9e599fb66
hadalzonee <alexhadalzone@gmail.com>
2025-05-31 16:28:18 -0700

    test creating object from json string

--- pokesim/Program.cs
+++ pokesim/Program.cs
@@ -8,11 +8,26 @@
  */
 
 using System;
+using System.Text.Json;
 
 namespace PokeSim {
+    public class DexEntry {
+        public String Name { get; set; }
+    }
+
     internal class Program { 
         static void Main(string[] args) {
+            string jsonTest =
+                """
+                {
+                    "Name": "Bulbasaur"
+                }
+                """;
+            DexEntry? bulbasaur = JsonSerializer.Deserialize<DexEntry>(jsonTest);
+
             Console.WriteLine("ARGO'S POKEMON BATTLE SIMULATOR");
+            Console.WriteLine("Now testing pulling value from JSON string.");
+            Console.WriteLine($"Name pulled: {bulbasaur?.Name}");
         }
     }
 }
