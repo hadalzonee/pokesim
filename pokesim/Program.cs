291058678bed89e7be899662ed7210844e997f6f
hadalzonee <alexhadalzone@gmail.com>
2025-05-31 18:08:36 -0700

    edited comments

--- pokesim/Program.cs
+++ pokesim/Program.cs
@@ -15,7 +15,17 @@ namespace PokeSim {
             DexEntry? bulbasaur = JsonSerializer.Deserialize<DexEntry>(jsonTest2);
 
             Console.WriteLine("ARGO'S POKEMON BATTLE SIMULATOR");
-            Console.WriteLine($"Current directory: {Environment.CurrentDirectory}");
+            Console.WriteLine($"Name: {bulbasaur?.Name}");
         }
     }
 }
+
+/*
+ * so right now i'm trying to figure out the best way to read in SPECIFIC dex entries from a json file.
+ * i really don't want to have to create an individual dex entry file for each pokemon, that'll get out
+ * of hand very quickly. ideally, i want each generation to have a "pokedex" file, which i can search through
+ * until i find the desired dex entry, then create a dexentry object from there. i'm not really sure this is
+ * possible. the next best option would be to create an array or a list (iirc it has to be a list, i don't
+ * think you can have arrays of objects?) that contains the entire pokedex, which i can then search through
+ * to find the desired dex entry.
+ */
