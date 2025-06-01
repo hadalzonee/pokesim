b267b0fe698b47825bd96eec1cbb0b4131693558
hadalzonee <alexhadalzone@gmail.com>
2025-05-31 17:51:06 -0700

    finally got reading from a file working

--- pokesim/Program.cs
+++ pokesim/Program.cs
@@ -1,13 +1,10 @@
 ﻿/*
  * argo's command line pokemon battle simulator
  * version 0.0.1
- * current features:
- *      nothing
- * planned features:
- *      everything
  */
 
 using System;
+using System.IO;
 using System.Text.Json;
 
 namespace PokeSim {
@@ -24,13 +21,12 @@ namespace PokeSim {
                     "Weight": 6.9
                 }
                 """;
-            DexEntry? bulbasaur = JsonSerializer.Deserialize<DexEntry>(jsonTest);
+            string fileName = "bulbasaur.json";
+            string jsonTest2 = File.ReadAllText(fileName);
+            DexEntry? bulbasaur = JsonSerializer.Deserialize<DexEntry>(jsonTest2);
 
             Console.WriteLine("ARGO'S POKEMON BATTLE SIMULATOR");
-            Console.WriteLine("Now testing pulling multiple values from JSON string.");
-            Console.WriteLine($"ID and Name: {bulbasaur?.ID} {bulbasaur?.Name}");
-            Console.WriteLine($"Type(s): {bulbasaur?.Type1}/{bulbasaur?.Type2}");
-            Console.WriteLine($"Height: {bulbasaur?.Height}. Weight: {bulbasaur?.Weight}");
+            Console.WriteLine($"Current directory: {Environment.CurrentDirectory}");
         }
     }
 }
